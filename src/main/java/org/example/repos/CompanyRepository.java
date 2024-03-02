da85c798d40d49ccccbfb253fa734a85fc281405
Konstantin <k.bobkov1408@gmail.com>
2024-03-02 16:24:11 +0300

    create repos and set settings for connection with DB

---
+++ src/main/java/org/example/repos/CompanyRepository.java
@@ -0,0 +1,7 @@
+package org.example.repos;
+
+import org.example.model.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompanyRepository extends JpaRepository<Company,Long> {
+}
---
+++ src/main/java/org/example/repos/ProductRepository.java
@@ -0,0 +1,8 @@
+package org.example.repos;
+
+import org.example.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
+    Product findByName(String name);
+}
